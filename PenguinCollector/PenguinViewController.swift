1f29e678b54d39274196820daedb658820ea3dcc
tzits <tzitsman@gmail.com>
2017-06-26 12:56:05 -0400

    adds image for image viewer and connects to viewcontroller

---
+++ PenguinCollector/PenguinViewController.swift
@@ -0,0 +1,31 @@
+//
+//  PenguinViewController.swift
+//  PenguinCollector
+//
+//  Created by Toby Zitsman on 6/26/17.
+//  Copyright © 2017 Toby Zitsman. All rights reserved.
+//
+
+import UIKit
+
+class PenguinViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    @IBOutlet weak var penguinImageView: UIImageView!
+    
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+}
